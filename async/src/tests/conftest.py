135cccffb25b9638a6f399ed8039ed7b383d3a64
purwowd <id.purwowd@gmail.com>
2020-10-26 12:58:41 +0700

    Refactor conftest.py

--- async/src/tests/conftest.py
+++ async/src/tests/conftest.py
@@ -3,13 +3,8 @@ from starlette.testclient import TestClient
 
 from app.main import app
 
+
 @pytest.fixture(scope="module")
 def test_app():
     client = TestClient(app)
     yield client
-
-
-def test_ping(test_app):
-    res = test_app.get("/ping")
-    assert res.status_code == 200
-    assert res.json == {"ping": "pong!"}
